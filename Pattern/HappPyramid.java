9db434b0fe609bf309be55b179e434a0e2d80c5e
Satyajit Routray <23cse596.satyajitroutray@giet.edu>
2026-03-17 09:25:29 +0530

    Add files via upload

---
+++ Pattern/HappPyramid.java
@@ -0,0 +1,11 @@
+public class HappPyramid {
+    public static void main(String a[]){
+        int n=4;
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
