9844332f55878d735425ab787b9ece8237938fa3
Satyajit Routray <23cse596.satyajitroutray@giet.edu>
2026-03-17 09:28:43 +0530

    Add files via upload

---
+++ Rough/Typecasting.java
@@ -0,0 +1,7 @@
+class Typecasting {
+public static void main(String arge []){
+    float a=90.99f;
+    int b=(int)a;
+    System.out.println(b);
+}
+}
---
+++ Rough/year.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class year{
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+System.out.print("Input the year: ");
+int year = sc.nextInt();
+boolean x = (year % 4) == 0;
+boolean y = (year % 100) != 0;
+boolean z = ((year % 100 == 0) && (year % 400 == 0));
+System.out.println(x);
+System.out.println(y);
+System.out.println(z);
+}
+}
