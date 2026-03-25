53f0400ccc4922c511447ba0e695c2f88747cf71
Satyajit Routray <23cse596.satyajitroutray@giet.edu>
2026-03-25 21:42:32 +0530

    Add files via upload

---
+++ Pattern/HoloRhombus.java
@@ -0,0 +1,21 @@
+class HoloRhombus{
+    public static void ho(int n){
+        for (int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=n;j++){
+                if(i==1 || i==n || j==1 || j==n){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String A[]){
+        ho(5);
+    }
+}
